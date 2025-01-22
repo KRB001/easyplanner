5e03e062734a900e029143d9f1d79961c56ed66d
KRB001 <kieranbentley0@gmail.com>
2025-01-22 10:59:24 -0500

    read in from log, output basic info

--- main.py
+++ main.py
@@ -1,3 +1,36 @@
+# imports
 from reader import *
+from datetime import datetime, timedelta
+import calendar
 
+# ansii codes
+BOLD = "\033[1m"
+RESET = "\033[0m"
+YELLOW = "\033[33m"
+
+# data assignment
 data = read_in("log.txt")
+tzoffset = data[0]
+entries = data[1:]
+
+# set greet phrase
+curr = datetime.utcnow() + timedelta(hours=tzoffset)
+time_of_day = ""
+if 0 <= curr.hour < 12:
+    time_of_day = "morning"
+elif 12 <= curr.hour < 17:
+    time_of_day = "afternoon"
+else:
+    time_of_day = "evening"
+
+# header
+for i in range(20):
+    print("\n")
+print("#### WELCOME TO EASY PLANNER ####\n")
+print("Good " + time_of_day + "! Your tasks are listed below.\n")
+print("#################################\n")
+
+inc = 0
+for entry in entries:
+    inc = inc + 1
+    print("[" + entry[1] + "] " + str(inc) + ". " + YELLOW + entry[0] + RESET + " - " + entry[2])
--- reader.py
+++ reader.py
@@ -2,8 +2,8 @@ def read_in(filename):
     f = open(filename)
     lines = f.readlines()
 
-    data = []
-    for line in lines:
+    data = [int(lines[0].replace("\n",""))]
+    for line in lines[1:]:
         data.append(line.replace("\n","").split(","))
 
     return data
