577b8bce145d7d23d136ad8538776d0163edf6d2
KRB001 <kieranbentley0@gmail.com>
2025-02-04 11:45:53 -0500

    dump no longer purges same-day entries

--- utilities.py
+++ utilities.py
@@ -9,7 +9,7 @@ def dump(dest, tzoffset, mode, entries):
         if entry[3] == "No time limit":
             if entry[1] == "Complete":
                 finished_entries.append(entry)
-        elif datetime.now() > datetime.strptime(entry[3], "%Y-%m-%d"):
+        elif datetime.now() > datetime.strptime(entry[3], "%Y-%m-%d") + timedelta(days=1):
             finished_entries.append(entry)
         else:
             ongoing_entries.append(entry)
